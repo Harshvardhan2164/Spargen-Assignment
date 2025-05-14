36fb5aed3eec38862af8771783dcdc93403947a2
Harshvardhan Sharma <harshvardhan22102@iiitnr.edu.in>
2025-05-15 00:00:28 +0530

    Backend updated

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,13 +1,15 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
 
-export const protect = (req, res, next) => {
+export const protect = async (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
 
     if(!token) return res.status(401).json({ message: 'Unauthorized' });
 
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        req.user = await User.findById(decoded.id).select('-password');
+        
         next();
     } catch(error){
         res.status(500).json({ message: 'Invalid token' });
