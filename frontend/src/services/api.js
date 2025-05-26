335f9f7e8ca7524d827674fcaa0f2f38f921cda0
Harshvardhan Sharma <harshvardhan22102@iiitnr.edu.in>
2025-05-26 16:54:07 +0530

    Render route added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://homecraft-backend-eos4.onrender.com/api',
     withCredentials: true,
 });
 
